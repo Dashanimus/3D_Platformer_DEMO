c538b42a4d315901a087133d9f44fbb4cebfc2ff
◇°• Dashanimus •°◇ <dashanimus@gmail.com>
2025-05-11 22:53:45 +0300

    Lesson 5 Commit

--- scn/levels/level1/icon.gd
+++ scn/levels/level1/icon.gd
@@ -7,13 +7,32 @@ var hearts = 3.5   # Переменная float.
 const SPEED = 2  # Константа, постоянна и не меняется.
 var x = coins / SPEED  # Можно комбинировать переменные и константы.
 # " = " это оператор присвоения, " == " это оператор равно.
+var is_godot_awesome = true  # Инициализируем как true
+var key_collected = false
 
 
 # Вызывается единожды при первом открытии сцены.
 func _ready() -> void:
-	pass
+	print(add_these_numbers(11, 12))  # Переменные x и y.
 
 
 # Вызывается каждый фрейм в течение 60 сек.
 func _process(delta: float) -> void:
-	rotate_y(deg_to_rad(SPEED)) # Переводим градусы в радианы.
+	#minion()
+	check_inputs()
+
+
+func minion():  # Функция - подпрограмма в большой программе.
+	print("BANANA!")
+
+
+func add_these_numbers(x, y):   # Внутри переменные - параметры функции.
+	var sum = x + y
+	return sum
+
+
+func check_inputs():
+	if Input.is_action_pressed("ui_left"):
+		rotate_y(deg_to_rad(-SPEED)) # Движение на левую сторону.
+	elif Input.is_action_pressed("ui_right"):
+		rotate_y(deg_to_rad(SPEED)) # Переводим градусы в радианы.
--- scn/objects/player/player.gd
+++ scn/objects/player/player.gd
@@ -5,7 +5,7 @@ const SPEED = 5.0
 const JUMP_VELOCITY = 4.5
 
 
-func _physics_process(delta: float) -> void:
+func _physics_process(delta: float) -> void:  # это метод, т.к. он внутри CharacterBody3D.
 	# Add the gravity.
 	if not is_on_floor():
 		velocity += get_gravity() * delta
